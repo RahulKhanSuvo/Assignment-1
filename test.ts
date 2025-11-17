18ac7fecefa7de0e962209bc26c65aa5692798a3
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-11-17 22:49:28 +0600

    Problem 6 done

--- test.ts
+++ test.ts
@@ -1,8 +1,9 @@
 
-const users = [
-    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
+const myBook: Book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    publishedYear: 1925,
+    isAvailable: true,
+};
 
-console.log(filterActiveUsers(users));
+printBookDetails(myBook);
