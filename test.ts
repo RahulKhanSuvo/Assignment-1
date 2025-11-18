903961a6619dea6f659c82d2909981721b64e0a5
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-11-18 19:44:06 +0600

    remove test file

--- test.ts
+++
@@ -1,8 +0,0 @@
-
-const products = [
-    { name: "Pen", price: 10, quantity: 2 },
-    { name: "Notebook", price: 25, quantity: 3, discount: 10 },
-    { name: "Bag", price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products)); // 127.5
